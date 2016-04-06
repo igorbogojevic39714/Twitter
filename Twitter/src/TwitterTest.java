f1b436d2fb03ebd96e92481231c6b0b49d4f3838
igorbogojevic39714 <bogojevic.igor@gmail.com>
2016-04-06 14:06:26 +0200

    Uradjene test za sve metode u klasi Twitter i ispravljene neke greske u kodu u klasi, da bi radili testovi.

---
+++ Twitter/src/TwitterTest.java
@@ -0,0 +1,76 @@
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+import org.junit.After;
+
+import org.junit.Before;
+
+import org.junit.Test;
+
+import com.twitter.Twitter;
+import com.twitter.poruke.TwitterPoruka;
+
+public class TwitterTest {
+	Twitter t;
+	TwitterPoruka tp1 = new TwitterPoruka();
+	TwitterPoruka tp2 = new TwitterPoruka();
+	@Before
+	public void setUp() throws Exception {
+		t = new Twitter();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		t= null;
+	}
+
+	@Test 
+	public void testVratiSvePoruke() {
+		t.unesi("Igor", "Nova poruka od Igora");
+		t.unesi("Zoran", "Nova poruka od Zorana");
+		
+		tp1.setKorisnik("Igor");
+		tp1.setPoruka("Nova poruka od Igora");
+		tp2.setKorisnik("Zoran");
+		tp2.setPoruka("Nova poruka od Zorana");
+		
+		LinkedList<TwitterPoruka> novaLista = new LinkedList<TwitterPoruka>();
+		novaLista.addLast(tp1);
+		novaLista.addLast(tp2);
+		//novaLista = t.vratiSvePoruke();
+		assertEquals (  novaLista, t.vratiSvePoruke()  );
+		
+		
+	}
+	
+	@Test 
+	public void testUnesi() {
+		t.unesi("Igor", "Nova poruka od Igora");
+		
+		
+		tp1.setKorisnik("Igor");
+		tp1.setPoruka("Nova poruka od Igora");
+		
+		
+		LinkedList<TwitterPoruka> novaLista = new LinkedList<TwitterPoruka>();
+		novaLista.addLast(tp1);
+		
+		//novaLista = t.vratiSvePoruke();
+		assertEquals ( novaLista, t.vratiSvePoruke()  );
+		
+	}
+	@Test
+	public void testVratiSvePorukeNiz (){
+		t.unesi("Igor2", "Nova poruka Igor2");
+		t.unesi("Igor2", "Tag Nova poruka Igor2");
+		TwitterPoruka[] noviNiz = new TwitterPoruka[1];
+		tp1.setKorisnik("Igor2");
+		tp1.setPoruka("Tag Nova poruka Igor2");
+		
+		noviNiz[0] = tp1;
+		TwitterPoruka[] praviNiz = t.vratiPoruke(1, "Tag");
+		assertArrayEquals(noviNiz, praviNiz);
+	}
+
+}
--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -28,7 +28,7 @@ public class Twitter {
 	public void unesi(String korisnik, String poruka) {
 			//Pravi se nova poruka i puni podacima.
 			TwitterPoruka tp = new TwitterPoruka();
-			tp.setKorisnik("korisnik");
+			tp.setKorisnik(korisnik);
 			tp.setPoruka(poruka);
 			//Poruka se unosi u listu na kraj
 			poruke.addLast(tp);
@@ -39,6 +39,7 @@ public class Twitter {
 	 * @param maxBroj Maksimalni broj elemenata u nizu.
 	 * @param tag Tag na osnovu koga se trazi u listi.
 	 * @return rezultat
+	 * @throws RuntimeException
 	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 			if (tag==null || tag == "")
@@ -58,7 +59,7 @@ public class Twitter {
 			for (int i = 0; i < poruke.size(); i++)
 			if (poruke.get(i).getPoruka().indexOf(tag)!=-1)
 			if (brojac < maxBroj){
-			rezultat[brojac+1]=poruke.get(i);
+			rezultat[brojac]=poruke.get(i);
 			brojac++;
 			}
 			else break;
